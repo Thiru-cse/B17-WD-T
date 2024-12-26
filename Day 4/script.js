e1e9f20b0760b98bed361d10ccea2bfc230020e1
Thiru <69345132+Thiru-cse@users.noreply.github.com>
2024-12-26 21:19:29 +0530

    Add files via upload

---
+++ Day 4/script.js
@@ -0,0 +1,19 @@
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+}
+
+console.log(person.name);
+console.log(Object.keys(person));
+console.log(Object.values(person));
+person.gender = "male";
+console.log(person);
+
+// JSON - JavaScript Object Notation:
+{
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
