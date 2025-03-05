b3f1ced9434a085fcdfb1c08f7a3003e7fc2471c
Thiru <69345132+Thiru-cse@users.noreply.github.com>
2025-03-05 07:09:42 +0530

    Add files via upload

---
+++ React JS/React Project/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-center py-10'>
+      <h1 className='text-4xl font-bold'>Welcome to B17 Store!</h1>
+      <p className='mt-4 text-gray-600'>Shop your favorite products here!</p>
+    </div>
+  )
+}
+
+export default Home
