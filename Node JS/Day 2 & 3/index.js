58c313094db10aa151be8e56b231f8e25ce71dcb
Thiru <69345132+Thiru-cse@users.noreply.github.com>
2025-03-12 11:26:12 +0530

    Add files via upload

---
+++ Node JS/Day 2 & 3/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import employeeRouter from './Routers/employeeRouter.js';
+
+const app = express();
+const PORT = 4002;
+app.use(express.json());
+
+app.use("/api", employeeRouter);
+
+app.get('/', (req, res)=>{res.status(200).send('API is Working')});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
